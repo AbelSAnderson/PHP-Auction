1448d95ce2720b813b125586a806fc07de3a4aab
AbelAnderson <Asatheabel@gmail.com>
2019-11-20 11:10:51 -0500

    Updated User to send emails

--- app/Models/User.php
+++ app/Models/User.php
@@ -6,6 +6,9 @@ namespace App\Models;
 
 use App\Lib\Model;
 use App\Exceptions\ClassException;
+use App\Exceptions\MailException;
+use App\Lib\Logger;
+use App\Lib\Mail;
 
 /**
  * Class User
@@ -79,5 +82,31 @@ class User extends Model {
         $this->username = $username;
         $this->password = password_hash($password, PASSWORD_BCRYPT, ['cost' => 10]);
         $this->email = $email;
+        $this->verify = $this->randStr();
+    }
+
+    public function randStr(): string {
+        return substr(md5(rand()), 0, 16);
+    }
+
+    public function mailUser(): bool {
+        $verifystring = urlencode($this->verify);
+        $email = urlencode($this->email);
+        $url = CONFIG_URL;
+        $mail_body = <<<_MAIL_
+Hi $this->username,\n\n
+
+Please click on the following link to verify your new account:
+<a href="{$url}/verify.php?email=$email&verify=$verifystring">Click here</a>
+
+
+_MAIL_;
+
+        try {
+            return Mail::sendMail($this->email, CONFIG_AUCTIONNAME . " user verification", $mail_body);
+        } catch (MailException $e) {
+            Logger::getLogger()->critical("could not send mail: ", ['exception' => $e]);
+        }
+
     }
 }
